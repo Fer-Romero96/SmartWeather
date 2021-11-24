9f92867365896a85ecf0037a895fe0e9238622e8
Fernando Romero <fer_romero96@hotmail.com>
2021-11-23 18:43:38 -0600

    Finished SplashScreen

--- app/src/main/java/com/example/smartweather/SplashActivity.kt
+++ app/src/main/java/com/example/smartweather/SplashActivity.kt
@@ -4,16 +4,35 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import com.airbnb.lottie.LottieDrawable
+import com.example.smartweather.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
 
+        // Data Binding
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        animation()
+
+        // Delay the Main Activity
         Handler().postDelayed({
             startActivity(Intent(this, MainActivity::class.java))
             finish()
-        }, 3000)
+        }, 5000)
 
+    }
+
+    // Function for Lottie Animation
+    private fun animation(){
+        binding.animationView.setAnimationFromUrl("https://assets3.lottiefiles.com/private_files/lf30_jmgekfqg.json")
+        binding.animationView.repeatCount = LottieDrawable.INFINITE
+        binding.animationView.playAnimation()
     }
 }
